6f98ae70b4ee22e8e42ee63c13db9f973c9d1a34
uauyelbe <33121768+uauyelbe@users.noreply.github.com>
2018-03-02 11:42:27 +0600

    Update churn.py

--- churn.py
+++ churn.py
@@ -1,44 +1,102 @@
-from __future__ import print_function
-from xgboost import  XGBClassifier
-from xgboost import plot_tree
-import matplotlib.pyplot as plt
-from numpy import loadtxt
-import sys
-from io import StringIO
-from numpy import array
-from sklearn import datasets, linear_model
-from sklearn.model_selection import train_test_split
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn import tree
-
-import os
-import subprocess
-
-import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
-def get_data():
-    df = pd.read_csv("dataset.csv")
-    return df
 
-df = get_data()
+#Function importing Dataset
+def importdata():
+    balance_data = pd.read_csv("matching.csv")
 
-X = df.values[::, 0:19]
-y = df.values[::, 20]
+    #Printing the dataswet shape
+    print("Dataset Lenght: ", len(balance_data))
+    print("Dataset Shape: ", balance_data.shape)
 
-X_train, Y_train, x_test, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    #Printing the dataset obseravtions
+    print("Dataset: ", balance_data.head())
+    return balance_data
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(X_train, Y_train)
-clf.predict(x_test, y_test)
 
-import graphviz
+#Function to split the dataset
+def splitdataset(balance_data):
+    #Seperating the target variable
+    X = balance_data.values[:, 1:19]
+    Y = balance_data.values[:, 20]
 
-dot_data = tree.export_graphviz(clf, out_file=None)
-graph = graphviz.Source(dot_data)
-print(graph)
+    #Spliting the dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.3, random_state=100)
 
-print(X_train)
-print(x_test)
+    return X, Y, X_train, X_test, y_train, y_test
+
+
+#Function to perform training with giniIndex.
+def train_using_gini(X_train, X_test, y_train):
+    #Creating the classifier object
+    clf_gini = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
+
+    #Performing training
+    clf_gini.fit(X_train, y_train)
+    return clf_gini
+
+
+#Function to perform training with entropy.
+def tarin_using_entropy(X_train, X_test, y_train):
+    #Decision tree with entropy
+    clf_entropy = DecisionTreeClassifier(
+        criterion="entropy", random_state=100, max_depth=3, min_samples_leaf=5)
+
+    #Performing training
+    clf_entropy.fit(X_train, y_train)
+    return clf_entropy
+
+
+#Function to make predictions
+def prediction(X_test, clf_object):
+    #Predicton on test with giniIndex
+    y_pred = clf_object.predict(X_test)
+    print("Predicted values:")
+    print(y_pred)
+    return y_pred
+
+
+#Function to calculate accuracy
+def cal_accuracy(y_test, y_pred):
+    print("Confusion Matrix: ",
+          confusion_matrix(y_test, y_pred))
+
+    print("Accuracy : ",
+          accuracy_score(y_test, y_pred) * 100)
+
+    print("Report : ",
+          classification_report(y_test, y_pred))
+
+
+#Driver code
+def main():
+    #Building Phase
+    data = importdata()
+    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    clf_gini = train_using_gini(X_train, X_test, y_train)
+    clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
+
+    #Operational Phase
+    print("Results Using Gini Index:")
+
+    #Prediction using gini
+    y_pred_gini = prediction(X_test, clf_gini)
+    cal_accuracy(y_test, y_pred_gini)
+
+    print("Results Using Entropy:")
+    #Prediction using entropy
+    y_pred_entropy = prediction(X_test, clf_entropy)
+    cal_accuracy(y_test, y_pred_entropy)
+
+
+#Calling main function
+if __name__ == "__main__":
+    main()
